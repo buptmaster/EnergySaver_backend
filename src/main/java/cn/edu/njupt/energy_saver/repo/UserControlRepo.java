6e530ee1926b8cf1321bc9978902bb017a92e966
欧阳宇翔 <768471488@qq.com>
2020-05-27 21:26:32 +0800

    update ui

--- src/main/java/cn/edu/njupt/energy_saver/repo/UserControlRepo.java
+++ src/main/java/cn/edu/njupt/energy_saver/repo/UserControlRepo.java
@@ -4,6 +4,8 @@ import cn.edu.njupt.energy_saver.dataobject.UserControl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface UserControlRepo extends JpaRepository<UserControl, String> {
 
@@ -11,5 +13,6 @@ public interface UserControlRepo extends JpaRepository<UserControl, String> {
 
     UserControl findByAuthId(String authId);
 
+    @Transactional
     Integer deleteByUserName(String userName);
 }
